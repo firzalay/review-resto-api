abc4ccba88da0d05dab200be5cedb33841d53fe9
Wortelnich <firza7004@gmail.com>
2023-01-31 18:06:39 +0700

    feat:authentication

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tests\Feature\AuthenticationTest;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::post('/auth/login', [AuthenticationController::class, 'login'])->name('auth.login');
+Route::post('/auth/register', [AuthenticationController::class, 'register'])->name('auth.register');
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/auth/profile', [AuthenticationController::class, 'profile'])->name('auth.profile');
+    Route::get('/auth/logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
 });
+
+
+
+
+
